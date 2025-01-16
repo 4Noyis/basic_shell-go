c0b72e2314b173fb6ad60e585276b3a3d6ffe037
4Noyis <berenalpb@outlook.com>
2025-01-16 06:43:28 +0200

    ls command fix and color models added

--- commands/list_cmd/ls.go
+++ commands/list_cmd/ls.go
@@ -9,40 +9,55 @@ import (
 )
 
 func Ls(command models.Cmd) {
-
+	magenta := models.Magenta
+	reset := models.Reset
+	red := models.Red
 	// flagler ile özellikler eklenicek -l -a ...
-	// gizli dosyaları gizlemek için başında . var mı yok mu kontrol edilecek
-	// fazla dosya konumu verilirse hata döndürücek
 
 	if len(command.Args) == 0 {
 		file, _ := os.ReadDir("./")
-		wd, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Error getting working directory:", err)
-			return
-		}
-		wd = filepath.Base(wd)
-		fmt.Println(wd + ": ")
-		for _, file := range file {
-			if file.Name()[0] == '.' { // checking for secret files
-				continue
-			} else {
-
-				fmt.Fprintln(os.Stdout, "   └", file.Name())
+		run(file, "", magenta, reset)
+	} else {
+		for _, dirName := range command.Args {
+			files, err := os.ReadDir(dirName)
+			if err != nil {
+				fmt.Fprintln(os.Stdout, red, err, reset)
+				return
 			}
 
-		}
-	} else {
-		lastArg := command.Args[len(command.Args)-1]
-		file, _ := os.ReadDir(lastArg)
-		for _, file := range file {
-			fmt.Fprint(os.Stdout, file.Name()+"   ")
+			run(files, dirName, magenta, reset)
 		}
 	}
 
 	fmt.Println()
 }
 
-// func includeAll() {
+func run(file []os.DirEntry, lastArg string, color string, reset string) {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting working directory:", err)
+		return
+	}
 
-// }
+	if lastArg != "" {
+		wd = filepath.Base(wd)
+		fmt.Println(wd + "/" + lastArg + ": ")
+	} else {
+		wd = filepath.Base(wd)
+		fmt.Println(wd + ": ")
+	}
+
+	for _, file := range file {
+		if file.Name()[0] == '.' { // checking for secret files
+			continue
+		} else {
+
+			if file.IsDir() {
+				fmt.Fprintln(os.Stdout, color+"   └", file.Name()+reset)
+			} else {
+				fmt.Fprintln(os.Stdout, "   └", file.Name())
+			}
+
+		}
+	}
+}
